595f55fb2043a16d9e1ee7d86f68832b85d2cc21
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-08-01 08:29:46 -0700

    task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: tree
+ * Return: size
+ */
+ size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree && tree->parent != NULL)
+	{
+		return (binary_tree_depth(tree->parent) + 1)
+	}
+	return (0)
+}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -27,7 +27,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		h_left = binary_tree_height(tree->left);
 		h_right = binary_tree_height(tree->right);
-		return max(h_left, h_right) + 1;
+		if (h_left > h_right)
+			return (h_left + 1);
+		return (h_right + 1);
 	}
 	return(0);
 }
