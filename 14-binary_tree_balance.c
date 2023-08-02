12cd5179a908a913b2d2d96a64f419d1a64fb191
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-08-02 11:05:30 -0700

    i dont know why it workd

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,50 +1,31 @@
 #include "binary_trees.h"
-#include <stdio.h>
-int max(int, int);
-int max(int h_left, int h_right)
-{
-	if (h_left > h_right)
-		return (h_left);
-	return (h_right);
-}
-int check_binary_is_full(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	if (tree->left || tree->right)
-	{
-		int h_left, h_right = 0;
-
-		h_left = check_binary_is_full(tree->left);
-		if (h_left == -1)
-			return (-1);
-		h_right = check_binary_is_full(tree->right);
-		if (h_right == -1)
-			return (-1);
-
-		if (abs(h_left - h_right) > 1)
-		{
-			return (-1);
-		}
-		return (max(h_left, h_right) + 1);
-	}
-	return (0);
-}
 /**
- * binary_tree_is_full - checks if a binary tree is full
- *
- * @tree : tree
- * Return: int
+ * Function to calculate the height of a binary tree
  */
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	int result;
+int _binary_tree_height(const binary_tree_t *tree) {
+    int left_height, right_height;
 
-	result = check_binary_is_full(tree);
-	printf("%d result\n", result);
-	if (result > 0)
-	{
-		return (1);
-	}
-	return (0);
+    if (tree == NULL)
+        return -1;
+
+    left_height = _binary_tree_height(tree->left);
+    right_height = _binary_tree_height(tree->right);
+
+    return (left_height > right_height) ? (left_height + 1) : (right_height + 1);
+}
+
+/**
+ * Function to measure the balance factor of a binary tree
+ */
+int binary_tree_balance(const binary_tree_t *tree) {
+    int left_height;
+    int right_height;
+
+    if (tree == NULL)
+        return 0;
+
+    left_height = _binary_tree_height(tree->left);
+    right_height = _binary_tree_height(tree->right);
+
+    return left_height - right_height;
 }
---
+++ 14-binary_tree_balance1.c
@@ -0,0 +1,46 @@
+#include "binary_trees.h"
+#include <stdio.h>
+int max(int, int);
+int max(int h_left, int h_right)
+{
+	if (h_left > h_right)
+		return (h_left);
+	return (h_right);
+}
+int check_binary_is_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left || tree->right)
+	{
+		int h_left, h_right = 0;
+
+		h_left = check_binary_is_balance(tree->left);
+		if (h_left == -1)
+			return (-1);
+		h_right = check_binary_is_balance(tree->right);
+		if (h_right == -1)
+			return (-1);
+
+		if (abs(h_left - h_right) > 1)
+		{
+			return (-1);
+		}
+		return (max(h_left, h_right) + 1);
+	}
+	return (0);
+}
+/**
+ * binary_tree_balance- checks if a binary tree is full
+ *
+ * @tree : tree
+ * Return: int
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int result;
+
+	result = check_binary_is_balance(tree);
+	return (result);
+}
+
