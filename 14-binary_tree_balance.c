8e5fcc9fd120fdd8abf723e81529200d13c9d8b6
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-08-03 02:43:25 -0700

    spelling mistake

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -31,7 +31,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	left_height = binary_tree_height(tree->left);
-	right_heigh = binary_tree_height(tree->right);
+	right_height = binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
